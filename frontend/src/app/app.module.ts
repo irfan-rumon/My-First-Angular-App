8b0569077a16c41db500b67c6fa163d6ee984391
irfan-rumon <irfannisho8571@gmail.com>
2022-08-28 21:38:03 +0600

    Add parent container

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddFrameworkComponent } from './components/add-framework/add-framework.component';
+import { ParentComponent } from './components/parent/parent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddFrameworkComponent
+    AddFrameworkComponent,
+    ParentComponent
   ],
   imports: [
     BrowserModule,
