8bdd99b020dff30fadf38db8a5e064714450e785
irfan-rumon <irfannisho8571@gmail.com>
2022-08-29 10:48:19 +0600

    Add navigation

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ParentComponent } from './components/parent/parent.component';
+import { FrameworkItemComponent } from './components/framework-item/framework-item.component';
 
-const routes: Routes = [];
+  const routes: Routes = [
+    { path: '', component: ParentComponent ,
+    
+    }
+    
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
